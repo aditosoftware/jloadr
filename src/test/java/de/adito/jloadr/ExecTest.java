c9959d0ded0fc741d533c92400cca8c946598d5e
Johannes Boesl <j.boesl@adito.de>
2017-01-15 21:08:38 +0100

    Added execution to unit test.

---
+++ src/test/java/de/adito/jloadr/ExecTest.java
@@ -0,0 +1,18 @@
+package de.adito.jloadr;
+
+/**
+ * @author bo
+ *         Date: 14.01.17
+ *         Time: 19:32
+ */
+public class ExecTest
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println(args[0]);
+    System.err.println(args[1]);
+    System.out.println(System.getProperty("test"));
+  }
+
+}
--- src/test/java/de/adito/jloadr/Test_Loader.java
+++ src/test/java/de/adito/jloadr/Test_Loader.java
@@ -4,14 +4,19 @@ import de.adito.jloadr.api.*;
 import de.adito.jloadr.repository.jlr.JlrResourcePack;
 import de.adito.jloadr.repository.jnlp.JnlpResourcePack;
 import de.adito.jloadr.repository.local.LocalStore;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.*;
-import java.net.URL;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.MessageFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author j.boesl, 09.01.17
@@ -49,7 +54,7 @@ public class Test_Loader
   }
 
   @Test
-  public void testLoad() throws IOException, InterruptedException
+  public void testLoader() throws IOException, InterruptedException
   {
     List<String> fileNames = _createFileNames();
     _createTestFiles(dir, fileNames);
@@ -69,11 +74,34 @@ public class Test_Loader
     Thread.sleep(1000);
     localResourcePack = new Loader().load(localStore, remoteResourcePack, null);
     _check(remoteResourcePack, localResourcePack);
+
+
+    JLoaderConfig loaderConfig = new JLoaderConfig();
+    IStoreResource configResource = localResourcePack.getResource(JLoaderConfig.CONFIG_NAME);
+    Assert.assertNotNull(configResource);
+    try (InputStream inputStream = configResource.getInputStream())
+    {
+      loaderConfig.load(inputStream);
+    }
+
+    Process process = new ProcessBuilder(loaderConfig.getStartCommands())
+        .directory(store2Path.resolve(localResourcePack.getId()).toAbsolutePath().toFile())
+        .start();
+
+    process.waitFor(10, TimeUnit.SECONDS);
+    Assert.assertEquals(0, process.exitValue());
+
+    String sout = _readString(process.getInputStream());
+    String serr = _readString(process.getErrorStream());
+
+    Assert.assertEquals("arg1\ntrue\n", sout);
+    Assert.assertEquals("arg2\n", serr);
   }
 
   private void _check(IResourcePack pRemoteResourcePack, IResourcePack pLocalResourcePack) throws IOException
   {
-    for (IResource rResource : pRemoteResourcePack.getResources()) {
+    for (IResource rResource : pRemoteResourcePack.getResources())
+    {
       String id = rResource.getId();
       IResource lResource = pLocalResourcePack.getResource(id);
       Assert.assertNotNull("no local resource for '" + rResource.getId() + "'.", lResource);
@@ -87,20 +115,25 @@ public class Test_Loader
 
   private void _createTestFiles(File pDir, List<String> pNames) throws IOException
   {
-    for (String fileName : pNames) {
+    for (String fileName : pNames)
+    {
       File file = new File(pDir, fileName);
       file.createNewFile();
-      try (FileOutputStream outputStream = new FileOutputStream(file)) {
+      try (FileOutputStream outputStream = new FileOutputStream(file))
+      {
         for (int j = 0; j < 1024; j++)
           outputStream.write((int) (200 * Math.random()) + 1);
       }
     }
+    for (Path path : Files.newDirectoryStream(Paths.get("target"), "jloadr-*-tests.jar"))
+      Files.copy(path, dir.toPath().resolve("jloadr-tests.jar"));
   }
 
   private List<String> _createFileNames()
   {
     List<String> fileNames = new ArrayList<>();
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 20; i++)
+    {
       int number = (int) (Math.random() * i) + 1;
       String fileName = "testFile_" + number + ".txt";
       if (!fileNames.contains(fileName))
@@ -115,7 +148,8 @@ public class Test_Loader
     String res1 = "";
     String res2 = "";
     String refFormat = "<jar href=\"{0}\"/>";
-    for (int i = 0; i < pResources.size(); i++) {
+    for (int i = 0; i < pResources.size(); i++)
+    {
       String format = MessageFormat.format(refFormat, pResources.get(i));
       if (i < splitIndex)
         res1 += (res1.isEmpty() ? "" : "\n") + format;
@@ -129,15 +163,22 @@ public class Test_Loader
   private File _createJnlp(File pDir, String pName, String... pArgs) throws IOException
   {
     String content;
-    try (InputStream inputStream = Test_Loader.class.getResourceAsStream(pName + ".template")) {
-      java.util.Scanner s = new java.util.Scanner(inputStream).useDelimiter("\\A");
-      content = MessageFormat.format(s.hasNext() ? s.next() : "", (Object[]) pArgs);
+    try (InputStream inputStream = Test_Loader.class.getResourceAsStream(pName + ".template"))
+    {
+      content = MessageFormat.format(_readString(inputStream), (Object[]) pArgs);
     }
     File file = new File(pDir, pName);
-    try (PrintStream printStream = new PrintStream(new FileOutputStream(file))) {
+    try (PrintStream printStream = new PrintStream(new FileOutputStream(file)))
+    {
       printStream.write(content.getBytes("utf-8"));
     }
     return file;
+  }
+
+  private static String _readString(InputStream pInputStream)
+  {
+    java.util.Scanner s = new java.util.Scanner(pInputStream).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
   }
 
 
