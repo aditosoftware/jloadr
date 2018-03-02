917d9204f154cddcade3148dc8ff369deb703a08
j.boesl <j.boesl@adito.de>
2018-03-02 11:57:09 +0100

    Adjusted module dependencies to have a security provider included in generated jre. Adjusted jloadr-jre to work properly with jlink.

--- jloadr-bootstrap/src/main/java-mr/9/module-info.java
+++ jloadr-bootstrap/src/main/java-mr/9/module-info.java
@@ -1,5 +1,4 @@
 module de.adito.jloadr.bootstrap {
   requires java.base;
   requires java.desktop;
-  requires java.logging;
 }
--- jloadr/src/main/java-mr/9/module-info.java
+++ jloadr/src/main/java-mr/9/module-info.java
@@ -2,5 +2,5 @@ module de.adito.jloadr {
   requires java.base;
   requires java.xml;
   requires java.desktop;
-  requires java.logging;
+  requires jdk.crypto.cryptoki;
 }
